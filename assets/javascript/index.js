e7bb02236dd8887a7090ce260503740c934b8d90
CaioAbra <caioabra41@gmail.com>
2024-08-15 21:19:03 -0300

    faq

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -7,3 +7,29 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navBar.classList.toggle('active'); // Usa vanilla JavaScript para adicionar/remover a classe 'active'
 });
+
+$('.accordion-header').click(function () {
+    const target = $(this).data('target');
+    const $body = $(target);
+    const $header = $(this);
+    const arrowDown = `<img src="./src/img/VectorSetaUp.svg" alt="">`;
+    const arrowUp = `<img src="./src/img/VectorSetaDown.svg" alt="">`;
+
+    if ($body.hasClass('open')) {
+        $body.slideUp(300, function () {
+            $body.removeClass('open');
+            $header.removeClass('open');
+            $header.find('.icon').html(arrowUp);
+        });
+    } else {
+        $('.accordion-body').slideUp(300).removeClass('open');
+        $('.accordion-header').removeClass('open');
+        $('.accordion-header .icon').html(arrowUp);
+
+        $body.slideDown(300, function () {
+            $body.addClass('open');
+            $header.addClass('open');
+            $header.find('.icon').html(arrowDown);
+        });
+    }
+});
